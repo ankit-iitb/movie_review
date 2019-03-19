429e0cd5e3088b705dfac84fb6a7911576151b56
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2019-03-19 12:25:16 -0600

    Predict the review type for an actual review.

--- train_and_predict/src/main.rs
+++ train_and_predict/src/main.rs
@@ -7,7 +7,6 @@ use std::io::BufReader;
 
 use rusty_machine::learning::logistic_reg::LogisticRegressor;
 use rusty_machine::learning::SupModel;
-use rusty_machine::linalg::Matrix;
 use rusty_machine::linalg::Vector;
 
 use rusty_machine::data::tokenizers::NaiveTokenizer;
@@ -29,11 +28,7 @@ fn main() {
     // Data reading and pre-processing
     let training_file = File::open("../../movie_review/movie_data/full_train.txt")
         .expect("Something went wrong reading the file");
-    let test_file = File::open("../../movie_review/negative.txt")
-        .expect("Something went wrong reading the file");
-
     let training_buf_reader = BufReader::new(training_file);
-    let test_buf_reader = BufReader::new(test_file);
 
     let mut training_data: Vec<String> = Vec::new();
     for mut line in training_buf_reader.lines() {
@@ -70,8 +65,30 @@ fn main() {
     println!("Finish Training");
 
     // Predict
-    let new_point = Matrix::new(1, 1, vec![1.]);
+    let test_file = File::open("../../movie_review/negative.txt")
+        .expect("Something went wrong reading the file");
+    let test_buf_reader = BufReader::new(test_file);
+    let mut test_data: Vec<String> = Vec::new();
+    for mut line in test_buf_reader.lines() {
+        match line {
+            Ok(mut line) => {
+                test_data.push(line);
+            }
+
+            Err(_) => {
+                println!("Error in reading the data");
+            }
+        }
+    }
+
+    let mut freq_vectorizer = FreqVectorizer::<f64, NaiveTokenizer>::new(NaiveTokenizer::new());
+    let mut test_str: Vec<&str> = Vec::new();
+    for string in test_data.iter() {
+        test_str.push(string);
+    }
+    freq_vectorizer.fit(&test_str).unwrap();
+    let vectorized = freq_vectorizer.vectorize(&test_str).unwrap();
     let start = rdtsc();
-    let output = log_mod.predict(&new_point).unwrap();
+    let output = log_mod.predict(&vectorized).unwrap();
     println!("{}, CPU Cycles {}", output, rdtsc() - start);
 }
