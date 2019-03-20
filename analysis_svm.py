8da601b2229da5a644db7bea41db871346f14d8f
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2019-03-20 11:44:18 -0600

    Add SVM files for binary classification of movie review dataset.
    Used dump_svmlight_file(X,y,file) to create a svm file.

---
+++ analysis_svm.py
@@ -0,0 +1,26 @@
+from sklearn.datasets import load_svmlight_files
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+import numpy as np
+
+X_train, y_train, X_test, y_test = load_svmlight_files(("train_binary.feat", "test_binary.feat"))
+
+def train():
+    final_model = LogisticRegression(C=0.05, solver='lbfgs')
+
+    l,_ = X_train.shape
+    tr_idx = np.arange(l)
+    np.random.shuffle(tr_idx)
+    X_shuffle = X_train[tr_idx,:]
+    target_shuffle = np.array(y_train)[tr_idx]
+    final_model.fit(X_shuffle, target_shuffle)
+    return final_model
+
+if __name__ == "__main__":
+    final_model = train()
+
+    # Check accurancy
+    print ("Final Accuracy: %s"
+           % accuracy_score(y_test, final_model.predict(X_test)))
+
